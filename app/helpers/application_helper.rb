e6f05525fefa96ccb997fe3b253cf485f4a3a974
Shaun Hardin <hardin.124@buckeyemail.osu.edu>
2015-03-07 20:37:10 -0500

    Add bootstrap theme. Change how flash messages are shown.
    Still need to get devise flash messages to play nicely.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    flash_type = flash_type.to_sym
+    known_classes = {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-info'
+    }
+    known_classes[flash_type] || flash_type.to_s
+  end
 end
